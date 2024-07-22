c834e386f8f576ab5d14a3678ac72f400a4d8663
Turdugul <okonbaevaturdugul@gmail.com>
2024-07-22 12:41:05 +0100

    use advanced patterns, render props, compound components, and reusable modal window and context menu

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import { useForm } from 'react-hook-form'
 import { useCreateCabin } from './useCreateCabin'
 import { useUpdateCabin } from './useUpdateCabin'
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
     const { id: editId, ...editValue } = cabinToEdit
     const isEditSession = Boolean(editId)
 
@@ -32,6 +32,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                     onSuccess: (data) => {
                         console.log(data)
                         reset()
+                        onCloseModal?.()
                     },
                 }
             )
@@ -42,6 +43,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                     onSuccess: (data) => {
                         console.log(data)
                         reset()
+                        onCloseModal?.()
                     },
                 }
             )
@@ -52,7 +54,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Form
+            onSubmit={handleSubmit(onSubmit, onError)}
+            type={onCloseModal ? 'modal' : 'regular'}
+        >
             <FormRow label="Cabin name" error={errors?.name?.message}>
                 <Input
                     type="text"
@@ -146,7 +151,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button
+                    variation="secondary"
+                    type="reset"
+                    onClick={() => onCloseModal?.()}
+                >
                     Cancel
                 </Button>
                 <Button disabled={isWorking}>
